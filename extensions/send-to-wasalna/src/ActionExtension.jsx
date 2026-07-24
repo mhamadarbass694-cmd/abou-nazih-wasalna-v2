3453e4bdcf437fc38ed1fb29945b83bbb08b8706
Mohamad <abounazihstore@gmail.com>
2026-07-24 16:03:00 +0300

    latest-debug

--- extensions/send-to-wasalna/src/ActionExtension.jsx
+++ extensions/send-to-wasalna/src/ActionExtension.jsx
@@ -8,30 +8,34 @@ export default async () => {
 function Extension() {
   const { close, data } = shopify;
 
+  async function send() {
+    try {
+      const response = await fetch("/api/send-to-wasalna", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          orderId: data.selected[0].id,
+        }),
+      });
+
+      const result = await response.json();
+
+      console.log(result);
+
+      close();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <s-admin-action title="Send to Wasalna">
       <s-text>Order:</s-text>
       <s-text>{data.selected[0].id}</s-text>
 
-      <s-button
-        slot="primary-action"
-        onClick={async () => {
-          const res = await fetch("api/send-to-wasalna", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              orderId: data.selected[0].id,
-            }),
-          });
-
-          const result = await res.json();
-          console.log(result);
-
-          close();
-        }}
-      >
+      <s-button slot="primary-action" onClick={send}>
         Send to Wasalna
       </s-button>
     </s-admin-action>
